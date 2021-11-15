b442657816b3eafc57c5b6d882177e4ed4864cd4
Matthew Shaw <matshaw87@gmail.com>
2021-11-15 20:01:37 +0000

    first commit

--- react_challenge_start/react_challenge_start/src/containers/appointmentsPage/AppointmentsPage.js
+++ react_challenge_start/react_challenge_start/src/containers/appointmentsPage/AppointmentsPage.js
@@ -1,16 +1,27 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { AppointmentForm } from "../../components/appointmentForm/AppointmentForm";
+import { TileList } from '.../../components/tileList/TileList'
 
 export const AppointmentsPage = () => {
-  /*
-  Define state variables for 
-  appointment info
-  */
+ // Define Variables for appointment info
+ const appointments = props.appointments;
+ const contacts = props.contacts;
+ const addAppointment = props.addAppointment;
+ // states
+ const [title, setTitle] = useState(' ');
+ const [contact, setContact] = useState ('');
+ const [date, setDate] = useState(' ');
+ const [time, setTime] = useState
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    /*
-    Add contact info and clear data  
-    */
+    /addAppointment(title, contact, date, time);
+
+    setTitle(' ');
+    setContact(' ');
+    setDate(' ');
+    setTime(' ');
    
   };
 
@@ -18,6 +29,18 @@ export const AppointmentsPage = () => {
     <div>
       <section>
         <h2>Add Appointment</h2>
+        <AppointmentForm 
+        title={title}
+        setTitle={setTitle}
+        contact={contact}
+        setContact={setContact}
+        date={date}
+        time={time}
+        setTime={setTime}
+        handleSubmit={handleSubmit}
+        contacts={contacts}
+        />
+
       </section>
       <hr />
       <section>
